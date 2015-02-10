0e517a1c3c9fa90e1dd3daf728820c88c4950a9b
wpppl <w-f108@163.com>
2015-02-10 10:03:44 +0800

    修改写入方法

--- dlt9.py
+++ dlt9.py
@@ -91,7 +91,7 @@ def duijiang(i):
         #print(b)#显示字段com_number在my_number中的位置。
         c=number[0:132]+"\n"#显示下一个结果和前一个结果#修改处
         #print(c,file=fp1)#修改处
-	fp1.write(c)#修改处
+		fp1.write(c)#修改处
         print(c)
         #send('w-f108@163.com','wppplwang','13659398903@139.com','Use My Python',c) #邮件发送结果调用
         #send('w-f108@163.com','wppplwang','616895616@qq.com','Use My Python',c) #邮件发送结果调用
